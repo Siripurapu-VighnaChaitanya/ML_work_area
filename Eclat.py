b6978a4143fdbc6a00834e2903594e708e139192
Siripurapu-VighnaChaitanya <chaitanya659328@gmail.com>
2025-06-25 11:16:21 +0530

    Eclat.py file edited

--- Eclat.py
+++ Eclat.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 ds=pd.read_csv('Netflix Movie.csv',header=None)
 trans=[]
 for i in range(7464):
-    trans.append([str(ds.values[i][j]) for j in range(20) if str(ds.values[i][j] != 'NAN')])
-
+    trans.append([str(ds.values[i][j]) for j in range(20) if str(ds.values[i][j]) != 'nan'])
+print(trans)
 from apyori import apriori
 rules=apriori(transactions=trans,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2,max_length=2)
 
